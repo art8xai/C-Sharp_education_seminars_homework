ec54df62c45fdc27fb0f4beb303d9a10aa6830e7
Art <art8xai@gmail.com>
2022-09-02 19:17:32 +0700

    Added program for solving exercise 4

---
+++ 2022-08-30_C-Sharp_seminar1_HW/ex4/Program.cs
@@ -0,0 +1,29 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Write a program that takes three numbers as input and outputs the maximum of these numbers
+
+        // Definition of input numbers
+        Console.Write("Enter a first number: ");
+        int firstNumber = int.Parse(Console.ReadLine());
+        Console.Write("Enter a second number: ");
+        int secondNumber = int.Parse(Console.ReadLine());
+        Console.Write("Enter a third number: ");
+        int thirdNumber = int.Parse(Console.ReadLine());
+
+        // Introduced the variable "max" and conditionally preliminarily accept that the first input number is the maximum
+         var max = firstNumber;
+
+        // Compare of input numbers
+        if (secondNumber > max)
+        {
+            max = secondNumber;
+        }
+        if (thirdNumber > max)
+        {
+            max = thirdNumber;
+        }
+        Console.WriteLine($"max = {max}");
+    }
+}
