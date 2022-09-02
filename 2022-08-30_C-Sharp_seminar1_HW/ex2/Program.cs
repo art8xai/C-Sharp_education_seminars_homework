138a105ceaba254711a042e9c0a0560abe1474bf
Art <art8xai@gmail.com>
2022-09-02 19:17:05 +0700

    Added program for solving exercise 2

---
+++ 2022-08-30_C-Sharp_seminar1_HW/ex2/Program.cs
@@ -0,0 +1,31 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Write a program that takes two numbers as input and outputs which number is greater and which is less
+
+        // Definition of input numbers
+        Console.Write("Enter a first number: ");
+        int firstNumber = int.Parse(Console.ReadLine());
+        Console.Write("Enter a second number: ");
+        int secondNumber = int.Parse(Console.ReadLine());
+
+        // Compare of input numbers
+        if (firstNumber == secondNumber)
+        {
+            Console.WriteLine("The numbers are equal");
+        }
+        else
+        {
+            if (firstNumber > secondNumber)
+            {
+                Console.WriteLine($"min = {secondNumber}, max = {firstNumber}");
+            }
+            else
+            {
+                Console.WriteLine($"min = {firstNumber}, max = {secondNumber}");
+            }
+        }
+
+    }
+}
