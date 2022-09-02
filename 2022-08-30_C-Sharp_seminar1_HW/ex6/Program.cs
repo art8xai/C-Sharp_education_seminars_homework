61ee85b3187df7d7f9494b29afea7bb4410c41c7
Art <art8xai@gmail.com>
2022-09-02 19:17:54 +0700

    Added program for solving exercise 6

---
+++ 2022-08-30_C-Sharp_seminar1_HW/ex6/Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Write a program that takes a number as input and outputs whether the number is even (is it divisible by two without a remainder)
+
+        // Definition of input number
+        Console.Write("Enter a number to see if it's even: ");
+        int number = int.Parse(Console.ReadLine());
+
+        // Determine if a number is divisible by two without a remainder
+        if (number % 2 == 0)
+        {
+            Console.WriteLine("Yes");
+        }
+        else
+        {
+            Console.WriteLine("No");
+        }
+    }
+}
